7f7bef439dfaea366b55e866f4b89d2d49411258
chochimus <127354521+chochimus@users.noreply.github.com>
2024-10-10 19:18:21 -0400

    Backend completed

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(express.json());
+morgan.token("content", (req, res) => {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :content"
+  )
+);
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0
+      ? Math.max(...persons.map((person) => Number(person.id)))
+      : 0;
+  return String(maxId);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name) {
+    return response.status(400).json({ error: "name missing" });
+  } else if (!body.number) {
+    return response.status(400).json({ error: "number missing" });
+  } else if (persons.find((p) => p.name === body.name)) {
+    return response.status(409).json({ error: "user already exists" });
+  } else {
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: String(body.number),
+    };
+    persons = persons.concat(person);
+    response.json(person);
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((p) => p.id !== id);
+  response.status(204).end();
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  );
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
